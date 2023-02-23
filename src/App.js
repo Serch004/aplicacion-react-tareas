d72fb53e997f6b6c3582fb36d73f90d185bff142
Sergio Pichardo <ing.sergio.pichardo.a@gmail.com>
2023-02-23 16:26:26 -0600

    se realizo el contenedor de las listas y se le dio estilo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
         <img
         src={freeCodeCampLogo}
         className='freecodecamp-logo' />
-
+      </div>
+      <div className='tarea-lista-principal'>
+        <hi>Mis Tareas</hi>
+        
       </div>
     </div>
   );
