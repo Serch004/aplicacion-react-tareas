37dbd435665764d31be50b2d6510901831c57348
Sergio Pichardo <ing.sergio.pichardo.a@gmail.com>
2023-02-23 19:33:35 -0600

    se agrgaron nuevas propiedades id, completar tarea, eliminar tarea y sus funciones manejarCambio, manejarEnvio

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TareaFormulario from './TareaFormulario';
+import Tarea from './Tarea';
 import '../hojas-de-estilo/ListaDeTareas.css';
 
 function ListaDeTareas(){
+
+  const [tareas, setTareas] = useState([]);
+
+  const agrgarTarea = tarea =>{
+    console.log("tarea agregada");
+    console.log(tarea);
+  }
+
+
   return (
     <>
       <TareaFormulario />
       <div className='tareas-lista-contenedor'>
-        LISTA DE TAREAS
+        {
+          tareas.map((tarea) =>
+            <Tarea
+            texto={tarea.texto}
+            completada={tarea.completada}
+            />
+          )
+        }
       </div> 
     </>
   );
--- src/componentes/TareaFormulario.js
+++ src/componentes/TareaFormulario.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../hojas-de-estilo/TareaFormulario.css';
 
 function TareaFormulario (props){
+
+  const [input, setInput] = useState('');
+
+  const manejarCambio = e =>{
+    setInput(e.target.value);
+  }
+
+  const manejarEnvio = e => {
+    e.preventDefault();
+    
+    const tareaNueva = {
+      id: '34545',
+      texto: input
+    }
+  }
+
+
   return (
-    <form className='tarea-formulario'>
+    <form 
+    className='tarea-formulario'
+    onSubmit={manejarEnvio}>
       <input 
       className='tarea-input'
       type='texto'
       placeholder='Escribe una tarea'
       name='texto'
+      onChange={manejarCambio}
       />
       <button className='tarea-boton'>
         Agregar Tarea
